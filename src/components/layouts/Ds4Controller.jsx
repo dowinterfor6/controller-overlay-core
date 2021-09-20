66fff1556f07a1fe607d6127d54111c33c25625c
Andrew Chan <dowinterfor6@gmail.com>
2021-09-20 16:57:13 -0700

    Finish ds4 functionality, add ds4 specific preset keys, fix css

--- src/components/layouts/Ds4Controller.jsx
+++ src/components/layouts/Ds4Controller.jsx
@@ -35,7 +35,7 @@ const Ds4Controller = ({ state, handleLogoClick }) => {
   }, []);
 
   // TODO: This should be configurable somewhere maybe
-  const joystickOffsetCoefficient = 4;
+  const joystickOffsetCoefficient = 8;
 
   const { controllerColors } = state;
 
@@ -69,85 +69,217 @@ const Ds4Controller = ({ state, handleLogoClick }) => {
             id="touchpad"
             d="m 76.7395,96.169839 c 0,0 0.288412,-0.725387 2.261611,-0.652307 1.973202,0.07308 57.442069,0.146164 57.442069,0.146164 0,0 0.36541,-0.21925 1.75396,0.657733 M 76.7395,96.169839 76.222735,123.6617 c 0,0 0.05168,3.514 3.720703,3.56567 3.669027,0.0517 54.453462,0.22678 54.453462,0.22678 0,0 4.01948,0.21925 4.09256,-3.80024 0.0731,-4.01948 -0.29232,-27.332481 -0.29232,-27.332481"
             style={{
-              fill: controllerColors.body,
+              fill: pressedKey(17)
+                ? controllerColors.ds4Pressed
+                : controllerColors.ds4Body,
               stroke: controllerColors.stroke,
             }}
           />
         </g>
-        <g id="buttons">
+        {/* 
+          TODO: Buttons don't fade in because path doesn't support 
+          opacity or something?
+        */}
+        <g
+          id="ds4Buttons"
+          style={{
+            fill: controllerColors.mainButtons.background,
+            stroke: controllerColors.mainButtons.stroke,
+          }}
+        >
           <g id="triangle">
             <path
               id="triangleButton"
               d="m 173.37256,107.52005 a 6.4233413,6.4233413 0 0 1 -6.42334,6.42334 6.4233413,6.4233413 0 0 1 -6.42334,-6.42334 6.4233413,6.4233413 0 0 1 6.42334,-6.42334 6.4233413,6.4233413 0 0 1 6.42334,6.42334 z"
+              style={{
+                fill: pressedKey(3) ? controllerColors.mainButtons.pressed : "",
+              }}
             />
             <path
+              className="fade-in-path button-text y-button"
+              onAnimationEnd={buttonFadeInAnimationEnd}
               id="triangleIcon"
               d="m 166.926,102.67483 4.14507,7.17948 -8.29014,0 z"
+              style={{
+                fill: "none",
+                stroke: pressedKey(3)
+                  ? controllerColors.mainButtons.background
+                  : controllerColors.mainButtons.stroke,
+              }}
             />
           </g>
           <g id="circle">
             <path
               id="circleButton"
-              d="m 174.13288,121.07422 a -6.4233413,6.4233413 0 0 0 6.42334,6.42334 -6.4233413,6.4233413 0 0 0 6.42334,-6.42334 -6.4233413,6.4233413 0 0 0 -6.42334,-6.42334 -6.4233413,6.4233413 0 0 0 -6.42334,6.42334 z"
+              d="m 186.97956,121.07422 a -6.4233413,6.4233413 0 0 1 -6.42334,6.42334 -6.4233413,6.4233413 0 0 1 -6.42334,-6.42334 -6.4233413,6.4233413 0 0 1 6.42334,-6.42334 -6.4233413,6.4233413 0 0 1 6.42334,6.42334 z"
+              style={{
+                fill: pressedKey(1) ? controllerColors.mainButtons.pressed : "",
+              }}
             />
             <path
+              className="fade-in-path button-text circle-button"
+              onAnimationEnd={buttonFadeInAnimationEnd}
               id="circleIcon"
               transform="scale(-1,1)"
               d="m -176.60916,121.03757 a 3.9837828,3.9837828 0 0 1 -3.98378,3.98378 3.9837828,3.9837828 0 0 1 -3.98378,-3.98378 3.9837828,3.9837828 0 0 1 3.98378,-3.98379 3.9837828,3.9837828 0 0 1 3.98378,3.98379 z"
+              style={{
+                fill: "none",
+                stroke: pressedKey(1)
+                  ? controllerColors.mainButtons.background
+                  : controllerColors.mainButtons.stroke,
+              }}
             />
           </g>
           <g id="square">
             <path
               id="squareButton"
               d="m 159.77956,121.07422 a 6.4233413,6.4233413 0 0 1 -6.42334,6.42334 6.4233413,6.4233413 0 0 1 -6.42334,-6.42334 6.4233413,6.4233413 0 0 1 6.42334,-6.42334 6.4233413,6.4233413 0 0 1 6.42334,6.42334 z"
+              style={{
+                fill: pressedKey(2) ? controllerColors.mainButtons.pressed : "",
+              }}
             />
             <path
+              className="fade-in-path button-text square-button"
+              onAnimationEnd={buttonFadeInAnimationEnd}
               id="squareIcon"
               d="m 156.86292,124.50753 -6.86311,0 0,-6.86311 6.86311,0 z"
+              style={{
+                fill: "none",
+                stroke: pressedKey(2)
+                  ? controllerColors.mainButtons.background
+                  : controllerColors.mainButtons.stroke,
+              }}
             />
           </g>
           <g id="cross">
             <path
               id="crossButton"
               d="m 173.37256,134.62022 a 6.4233413,6.4233413 0 0 1 -6.42334,6.42334 6.4233413,6.4233413 0 0 1 -6.42334,-6.42334 6.4233413,6.4233413 0 0 1 6.42334,-6.42334 6.4233413,6.4233413 0 0 1 6.42334,6.42334 z"
+              style={{
+                fill: pressedKey(0) ? controllerColors.mainButtons.pressed : "",
+              }}
             />
             <path
+              className="fade-in-path button-text cross-button"
+              onAnimationEnd={buttonFadeInAnimationEnd}
               id="crossIcon"
               d="m 170.32118,131.24237 -6.73881,6.73881 m 0,-6.73881 6.73881,6.73881"
+              style={{
+                fill: "none",
+                stroke: pressedKey(0)
+                  ? controllerColors.mainButtons.background
+                  : controllerColors.mainButtons.stroke,
+              }}
             />
           </g>
         </g>
         <g id="topButtons">
-          <g transform="translate(0, -2.5)">
+          <g transform="translate(0, -2)">
             <path
+              className="no-draw"
               d="M 53.113152,79.243439 C 47.073264,79.241939 40.715563,91.343154 47.508175,91.33331 L 58.321244,91.31764 C 64.843123,91.30814 59.15304,79.244911 53.113152,79.243441 Z"
-              id="lt"
+              id="ltStroke"
+              style={{
+                stroke: controllerColors.topTriggers.lt.stroke,
+                strokeWidth: 0,
+                fill: controllerColors.topTriggers.lt.fill,
+              }}
             />
             <path
+              className="no-draw"
+              d="M 53.113152,79.243439 C 47.073264,79.241939 40.715563,91.343154 47.508175,91.33331 L 58.321244,91.31764 C 64.843123,91.30814 59.15304,79.244911 53.113152,79.243441 Z"
+              id="ltFill"
+              style={{
+                stroke: controllerColors.topTriggers.lt.stroke,
+                strokeWidth: 0,
+                opacity: hasAnimationEnded && state.keys[6].value,
+                fill:
+                  hasAnimationEnded &&
+                  state.keys[6].value &&
+                  controllerColors.pressed,
+              }}
+            />
+            <path
+              className="no-draw"
               d="m 162.68761,79.243439 c 6.03989,-0.0015 12.39759,12.099715 5.60498,12.089871 l -10.81307,-0.01567 c -6.52188,-0.0095 -0.8318,-12.072729 5.20809,-12.074199 z"
               id="rt"
+              style={{
+                stroke: controllerColors.topTriggers.rt.stroke,
+                strokeWidth: 0,
+                fill: controllerColors.topTriggers.rt.fill,
+              }}
+            />
+            <path
+              className="no-draw"
+              d="m 162.68761,79.243439 c 6.03989,-0.0015 12.39759,12.099715 5.60498,12.089871 l -10.81307,-0.01567 c -6.52188,-0.0095 -0.8318,-12.072729 5.20809,-12.074199 z"
+              id="rtFill"
+              style={{
+                stroke: controllerColors.topTriggers.rt.stroke,
+                strokeWidth: 0,
+                opacity: hasAnimationEnded && state.keys[7].value,
+                fill:
+                  hasAnimationEnded &&
+                  state.keys[7].value &&
+                  controllerColors.pressed,
+              }}
             />
           </g>
-          <path
-            d="m 151.71721,94.661367 c 0,0 2.21055,-1.146064 6.78053,-1.746926 2.7484,-0.361362 5.09833,-0.320656 7.3541,-0.353111 2.69533,-0.03878 6.3784,0.0828 8.59373,1.588467 0,0 -1.44952,-2.472024 -3.43484,-2.777097 -6.40234,-0.983812 -11.56754,-0.665199 -14.90862,-0.219247 -3.97988,0.531215 -4.3849,3.507914 -4.3849,3.507914 z"
-            id="rb"
-          />
-          <path
-            d="m 63.494479,94.66106 c 0,0 -2.21055,-1.146064 -6.78053,-1.746926 -2.7484,-0.361362 -5.09833,-0.320656 -7.3541,-0.353111 -2.69533,-0.03878 -6.3784,0.0828 -8.59373,1.588467 0,0 1.44952,-2.472024 3.43484,-2.777097 6.40234,-0.983812 11.56754,-0.665199 14.90862,-0.219247 3.97988,0.531215 4.3849,3.507914 4.3849,3.507914 z"
-            id="lb"
-          />
+          <g transform="translate(0, -1.35)">
+            <path
+              d="m 152.01608,96.089057 c 0,0 2.21055,-1.146064 6.78053,-1.746926 2.7484,-0.361362 5.09833,-0.320656 7.3541,-0.353111 2.69533,-0.03878 6.3784,0.0828 8.59373,1.588467 0,0 -1.44952,-3.251499 -3.43484,-3.556572 -6.40234,-0.983812 -11.29243,-0.71105 -14.63351,-0.265098 -3.97988,0.531215 -4.66001,4.33324 -4.66001,4.33324 z"
+              id="rb"
+              style={{
+                stroke: "none",
+                fill: pressedKey(5)
+                  ? controllerColors.pressed
+                  : controllerColors.topButtons.lb,
+              }}
+            />
+            <path
+              d="m 63.793344,96.08875 c 0,0 -2.21055,-1.146064 -6.78053,-1.746926 -2.7484,-0.361362 -5.09833,-0.320656 -7.3541,-0.353111 -2.69533,-0.03878 -6.3784,0.0828 -8.59373,1.588467 0,0 1.564149,-3.205647 3.549469,-3.51072 6.40234,-0.983812 11.475837,-0.802754 14.816917,-0.356802 3.97988,0.531215 4.361974,4.379092 4.361974,4.379092 z"
+              id="lb"
+              style={{
+                stroke: "none",
+                fill: pressedKey(4)
+                  ? controllerColors.pressed
+                  : controllerColors.topButtons.rb,
+              }}
+            />
+          </g>
         </g>
         <g id="centerButtons">
-          <path
-            id="ds4Button"
-            d="m 112.36963,150.12055 a 4.6508789,4.6508789 0 0 1 -4.65088,4.65088 4.6508789,4.6508789 0 0 1 -4.65088,-4.65088 4.6508789,4.6508789 0 0 1 4.65088,-4.65088 4.6508789,4.6508789 0 0 1 4.65088,4.65088 z"
-          />
+          <g
+            className={`${hasAnimationEnded ? "no-transition" : ""}`}
+            id="ds4Logo"
+            onClick={handleLogoClick}
+          >
+            <path
+              className={`fade-in-path no-draw`}
+              id="ds4Button"
+              d="m 112.36963,150.12055 a 4.6508789,4.6508789 0 0 1 -4.65088,4.65088 4.6508789,4.6508789 0 0 1 -4.65088,-4.65088 4.6508789,4.6508789 0 0 1 4.65088,-4.65088 4.6508789,4.6508789 0 0 1 4.65088,4.65088 z"
+              style={{ fill: controllerColors.logo.ds4Background }}
+            />
+            <path
+              className={`fade-in-path no-draw ${
+                hasAnimationEnded ? "no-transition" : ""
+              }`}
+              id="settingsIcon"
+              d="m 105.22023,150.44832 c -0.0139,-0.1045 -0.0209,-0.21247 -0.0209,-0.32741 0,-0.11146 0.007,-0.22291 0.0244,-0.32741 l -0.70706,-0.55032 c -0.0627,-0.0488 -0.0801,-0.1428 -0.0418,-0.21246 l 0.66874,-1.15637 c 0.0418,-0.0766 0.12887,-0.10101 0.2055,-0.0766 l 0.83245,0.33437 c 0.17415,-0.13235 0.35875,-0.24381 0.56425,-0.3274 l 0.12539,-0.8847 c 0.0139,-0.0836 0.0836,-0.1428 0.16719,-0.1428 h 1.33748 c 0.0836,0 0.14977,0.0592 0.16371,0.1428 l 0.12539,0.8847 c 0.2055,0.0836 0.39358,0.19853 0.56425,0.3274 l 0.83245,-0.33437 c 0.0766,-0.0279 0.1637,0 0.2055,0.0766 l 0.66526,1.15637 c 0.0418,0.0731 0.0279,0.1637 -0.0418,0.21246 l -0.70706,0.55032 c 0.0174,0.1045 0.0314,0.21944 0.0314,0.32741 0,0.10797 -0.007,0.22291 -0.0244,0.32741 l 0.70706,0.55032 c 0.0627,0.0488 0.0801,0.1428 0.0418,0.21246 l -0.66874,1.15637 c -0.0418,0.0766 -0.12887,0.10101 -0.2055,0.0766 l -0.83245,-0.33437 c -0.17415,0.13235 -0.35875,0.24381 -0.56425,0.3274 l -0.12539,0.8847 c -0.0174,0.0836 -0.0836,0.1428 -0.16719,0.1428 h -1.33748 c -0.0836,0 -0.15326,-0.0592 -0.16371,-0.1428 l -0.12539,-0.8847 c -0.2055,-0.0836 -0.39358,-0.19505 -0.56425,-0.3274 l -0.83245,0.33437 c -0.0766,0.0279 -0.1637,0 -0.2055,-0.0766 l -0.66874,-1.15637 c -0.0418,-0.0766 -0.0244,-0.1637 0.0418,-0.21246 z m 1.15547,-0.32844 a 1.3321103,1.3322436 0 0 0 1.33211,1.33224 1.3321103,1.3322436 0 0 0 1.33211,-1.33224 1.3321103,1.3322436 0 0 0 -1.33211,-1.33225 1.3321103,1.3322436 0 0 0 -1.33211,1.33225 z"
+              style={{
+                strokeWidth: 0,
+                fill: controllerColors.logo.ds4Foreground,
+                stroke: controllerColors.logo.ds4Foreground,
+              }}
+            />
+          </g>
           <path
             id="share"
             d="M 260.79102 371.17578 A 11.621475 9.218343 0 0 0 249.16992 380.39453 A 11.621475 9.218343 0 0 0 249.21875 381.24609 L 249.21875 400.3457 A 11.621475 9.218343 0 0 0 249.16992 401.18164 A 11.621475 9.218343 0 0 0 260.79102 410.40039 A 11.621475 9.218343 0 0 0 272.41211 401.18164 A 11.621475 9.218343 0 0 0 272.36328 400.3457 L 272.36328 381.23047 A 11.621475 9.218343 0 0 0 272.41211 380.39453 A 11.621475 9.218343 0 0 0 260.79102 371.17578 z "
             transform="scale(0.26458333)"
             style={{
-              fill: pressedKey(8) ? controllerColors.pressed : "",
+              fill: pressedKey(8)
+                ? controllerColors.mainButtons.pressed
+                : controllerColors.mainButtons.background,
               strokeWidth: 1, // TODO: Honestly not sure what happened to the SVG to make this necessary
             }}
           />
@@ -155,41 +287,55 @@ const Ds4Controller = ({ state, handleLogoClick }) => {
             id="menu"
             d="m 146.00075,98.206924 a 3.0748486,2.4390199 0 0 0 -3.07475,2.439126 3.0748486,2.4390199 0 0 0 0.0129,0.22531 v 5.05344 a 3.0748486,2.4390199 0 0 0 -0.0129,0.22117 3.0748486,2.4390199 0 0 0 3.07475,2.43913 3.0748486,2.4390199 0 0 0 3.07475,-2.43913 3.0748486,2.4390199 0 0 0 -0.0129,-0.22117 v -5.05757 a 3.0748486,2.4390199 0 0 0 0.0129,-0.22118 3.0748486,2.4390199 0 0 0 -3.07475,-2.439126 z"
             style={{
-              fill: pressedKey(9) ? controllerColors.pressed : "",
+              fill: pressedKey(9)
+                ? controllerColors.mainButtons.pressed
+                : controllerColors.mainButtons.background,
             }}
           />
         </g>
-        <g id="dpad">
+        <g
+          id="dpad"
+          style={{
+            fill: controllerColors.dpad.base,
+            stroke: controllerColors.dpad.ds4Stroke,
+          }}
+        >
           <path
             d="m 42.344289,105.59825 -0.0072,8.49975 5.472716,5.37844 5.478446,-5.39484 v -8.48335 z"
             id="up"
             style={{
-              fill: pressedKey(12) ? controllerColors.dpad.pressed : "",
+              fill: pressedKey(12) ? controllerColors.dpad.ds4Pressed : "",
             }}
           />
           <path
             d="m 42.344289,136.47688 -0.0072,-8.49975 5.472716,-5.37844 5.478446,5.39484 v 8.48335 z"
             id="down"
             style={{
-              fill: pressedKey(13) ? controllerColors.dpad.pressed : "",
+              fill: pressedKey(13) ? controllerColors.dpad.ds4Pressed : "",
             }}
           />
           <path
             d="m 32.274,115.56709 8.49975,-0.007 5.37844,5.47272 -5.39484,5.47845 H 32.274 Z"
             id="left"
             style={{
-              fill: pressedKey(14) ? controllerColors.dpad.pressed : "",
+              fill: pressedKey(14) ? controllerColors.dpad.ds4Pressed : "",
             }}
           />
           <path
             d="m 63.352883,115.56709 -8.49975,-0.007 -5.37844,5.47272 5.39484,5.47845 h 8.48335 z"
             id="right"
             style={{
-              fill: pressedKey(15) ? controllerColors.dpad.pressed : "",
+              fill: pressedKey(15) ? controllerColors.dpad.ds4Pressed : "",
             }}
           />
         </g>
-        <g id="joysticks">
+        <g
+          id="joysticks"
+          style={{
+            fill: controllerColors.joysticks.base,
+            stroke: controllerColors.joysticks.stroke,
+          }}
+        >
           <g id="leftJoystick">
             <path
               id="leftJoystickOuter"
@@ -205,7 +351,7 @@ const Ds4Controller = ({ state, handleLogoClick }) => {
                     }px, ${joystickOffsetCoefficient * state.axes.LY_AXIS}px)`
                   : "",
                 fill: pressedKey(10)
-                  ? controllerColors.joysticks.pressed
+                  ? controllerColors.ds4Pressed
                   : controllerColors.joysticks.body,
               }}
             />
@@ -225,7 +371,7 @@ const Ds4Controller = ({ state, handleLogoClick }) => {
                     }px, ${joystickOffsetCoefficient * state.axes.RY_AXIS}px)`
                   : "",
                 fill: pressedKey(11)
-                  ? controllerColors.joysticks.pressed
+                  ? controllerColors.ds4Pressed
                   : controllerColors.joysticks.body,
               }}
             />
--- src/presets/presets.js
+++ src/presets/presets.js
@@ -2,6 +2,8 @@ export const presets = {
   black: {
     displayName: "Black",
     body: "#121212",
+    ds4Body: "#121212",
+    ds4Pressed: "#fff",
     stroke: "#fff",
     pressed: "#fff",
     pressedInverse: "#121212",
@@ -11,11 +13,15 @@ export const presets = {
       base: "#121212",
     },
     dpad: {
+      ds4Stroke: "#fff",
+      ds4Pressed: "#fff",
       stroke: "#fff",
       pressed: "#fff",
       base: "#121212",
     },
     logo: {
+      ds4Foreground: "#fff",
+      ds4Background: "#121212",
       foreground: "#121212",
       background: "#fff",
     },
@@ -42,6 +48,8 @@ export const presets = {
   white: {
     displayName: "White",
     body: "#fff",
+    ds4Body: "#121212",
+    ds4Pressed: "#ddd",
     stroke: "#121212",
     pressed: "#121212",
     pressedInverse: "#fff",
@@ -51,11 +59,15 @@ export const presets = {
       base: "#121212",
     },
     dpad: {
+      ds4Stroke: "#121212",
+      ds4Pressed: "#ddd",
       stroke: "#fff",
       pressed: "#fff",
       base: "#121212",
     },
     logo: {
+      ds4Foreground: "#fff",
+      ds4Background: "#121212",
       foreground: "#fff",
       background: "#121212",
     },
@@ -82,6 +94,8 @@ export const presets = {
   blue: {
     displayName: "Blue",
     body: "#123066",
+    ds4Body: "#121212",
+    ds4Pressed: "#fff",
     stroke: "#fff",
     pressed: "#fff",
     pressedInverse: "#121212",
@@ -91,11 +105,15 @@ export const presets = {
       base: "#121212",
     },
     dpad: {
+      ds4Stroke: "#fff",
+      ds4Pressed: "#fff",
       stroke: "#fff",
       pressed: "#fff",
       base: "#121212",
     },
     logo: {
+      ds4Foreground: "#fff",
+      ds4Background: "#121212",
       foreground: "#121212",
       background: "#fff",
     },
@@ -122,6 +140,8 @@ export const presets = {
   red: {
     displayName: "Red",
     body: "#6e130e",
+    ds4Body: "#121212",
+    ds4Pressed: "#fff",
     stroke: "#fff",
     pressed: "#fff",
     pressedInverse: "#121212",
@@ -131,11 +151,15 @@ export const presets = {
       base: "#121212",
     },
     dpad: {
+      ds4Stroke: "#fff",
+      ds4Pressed: "#fff",
       stroke: "#fff",
       pressed: "#fff",
       base: "#121212",
     },
     logo: {
+      ds4Foreground: "#fff",
+      ds4Background: "#121212",
       foreground: "#121212",
       background: "#fff",
     },
